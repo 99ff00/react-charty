6e8e581fff36e9627be2b910064fb596a87a8a41
Aleksandr Telegin <99ff00@gmail.com>
2019-08-20 10:17:49 +0100

    Updates

---
+++ precache-manifest.56f8cf6c330e1a2e6c8a74cfbc211818.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2243d22754a64363e17a1e08ceeb2b3c",
+    "url": "/react-charty/index.html"
+  },
+  {
+    "revision": "b988e2b49479cd4a7aa1",
+    "url": "/react-charty/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "b572ab246172fefab532",
+    "url": "/react-charty/static/js/2.5679a868.chunk.js"
+  },
+  {
+    "revision": "b988e2b49479cd4a7aa1",
+    "url": "/react-charty/static/js/main.062406d1.chunk.js"
+  },
+  {
+    "revision": "10dfd52ae594f8c88ff0",
+    "url": "/react-charty/static/js/runtime~main.33b48a15.js"
+  }
+]);
